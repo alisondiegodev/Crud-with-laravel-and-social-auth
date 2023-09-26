b8496ae97ac3cc49613096705ea5f82ffa3c7d7c
Alison Diego <110138219+alisondiegodev@users.noreply.github.com>
2023-09-26 18:05:29 -0300

    working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{ asset('/style.css') }}">
     <link rel="stylesheet" href="{{ asset('/responsive.css') }}">
 
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
